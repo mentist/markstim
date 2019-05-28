b451a7f8e39552d611895ac8a791652768fa633b
Yong-Jun Lin <36145189+mentist@users.noreply.github.com>
2019-05-28 17:09:14 -0400

    Fleshed out handshake, command parsing, and command execution (old syntax). Added Protocol.txt.

--- MarkStim.ino
+++ MarkStim.ino
@@ -1,8 +1,11 @@
 /*
-Version: 2013-09-23~2018-01-10
+Version: 2013-09-23~2018-01-12
 Author: Yong-Jun Lin
 
 References:
+The official LED blink demo (File\Examples\Teensy\Tutorial1\Blink)
+The official EchoBoth demo (File\Examples\Teensy\Serial\EchoBoth)
+
 Definition of a TTL signal
   http://digital.natinst.com/public.nsf/$CXIV/ATTACH-AEEE-89LM9U/$FILE/TTL%20Specification.gif
 On Teensyduino, Serial.begin() actually accesses USB (12 Mbit/sec) so the baud rate does not matter.
@@ -11,6 +14,16 @@ On Teensyduino, Serial1.begin() accesses hardware serial UART. This is not usefu
   https://www.pjrc.com/teensy/td_uart.html
 Serial.send_now() and transmit buffering
   https://www.pjrc.com/teensy/td_serial.html#txbuffer
+Serial communication
+ http://arduino.cc/en/Tutorial/ReadASCIIString
+ http://arduinobasics.blogspot.com/2012/07/arduino-basics-simple-arduino-serial.html
+ http://playground.arduino.cc/interfacing/python
+ http://arduino.cc/en/Tutorial/SerialCallResponse
+ Getting string:
+ http://forum.arduino.cc/index.php/topic,45629.0.html
+ http://forum.arduino.cc/index.php?topic=41888.0
+Serial communication latency
+ http://neophob.com/2011/04/serial-latency-teensy-vs-arduino/
 
 History:
 2013-09-23
@@ -33,6 +46,8 @@ History:
  3. Reduced the test case to character echoing.
  4. Instead of echoing, respond by ++ to an incoming character.
  5. Tested whether CR+LF will be transmitted as part of the string.
+2018-01-12
+ 1. ﻿Handshake, parsing command, and executing command (old syntax since 2013)
 
 Future:
  1. Test if 1 ms TTL can trigger TMS pulses
@@ -63,34 +78,189 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 
+// Constants for debugging
+//#define DEBUGGING
+
+// Constants for memory management
+#define BUFSIZE 1025
+
 // Constants about hardware level wiring
 #if defined(__AVR_ATmega32U4__) // Teensy 2.0
 const unsigned int baudRate = 57600;
 const int pinLED = 11;
-const int pinSwitch1 = 10;
-const int pinSwitch2 = 9;
-const int pinResetButton = 6;
-//Avoid using pin 7 (RX) and 8 (TX) because these two are reserved for serial communication
 #elif defined(__AVR_AT90USB1286__)  // Teensy++ 2.0
+const unsigned int baudRate = 57600;
 const int pinLED = 6;
 #endif
 #define LED_ON HIGH
 #define LED_OFF LOW
 
+// State variables of the machine
+boolean bTeensyReady = false;
+boolean bTeensyRecving = false;
+boolean bTeensyExecing = false;
+
+// String buffer for serial communication
+char buffer[BUFSIZE] = "\0";
+unsigned int pBuffer = 0;
+
+// Command variables
+int nPeriods = 0;
+int periodOn_ms = 0;
+int periodOff_ms = 0;
+
 void setup()
 {
-  // Set serial communication (actually USB if using Teensy)
+  // Initialize serial communication (actually USB if using Teensy)
   Serial.begin(baudRate);
+  // Set pin mode
+  pinMode(pinLED, OUTPUT);
 }
 
 void loop()
 {
-  char incomingByte = '\0';
+  //Phase 1 (handshake): wait for the computer's '\n' and then reply 'Teensy ready\n'
+  //Phase 2 (settings): wait for a particular syntax and set execution variables (led on and off time)
+  //Phase 3 (execution): blink the LED accordinly
+  //An extension of this process has infinite phase 2 and phase 3 alternations
 
+  if (!bTeensyReady)
+  {
+    //LED on as state indicator
+    digitalWrite(pinLED, LED_ON);
+
+    Handshake();
+  }
+  else
+  {
+    //LED off as state indicator
+    digitalWrite(pinLED, LED_OFF);
+    
+    if (!bTeensyExecing)
+      RecvCmd();
+    else
+      ExecCmd();
+  }
+}
+
+void Handshake()
+{
+  // Wait for '\n' and reply with 'Teensy ready.'
+
+  char newByte = '\0';
   if (Serial.available())
   {
-    incomingByte = Serial.read();
-    Serial.println("This is a sentence with CR+LF.");
+    newByte = Serial.read();
+    if (newByte == '\n')
+    {
+      //Serial.flush();
+      Serial.println("Teensy ready");
+      memset(buffer, '\0', BUFSIZE);
+      bTeensyReady = true;
+    }
   }
+  return;
+}
+
+void RecvCmd()
+{
+  //Syntax: [{param1},{param2},{param3},...]
+
+  char newByte = '\0';
+
+  //Debug
+  if (Serial.available())
+  {
+    //The protocol here is that input data should be comma separated values followed by one new line character
+    newByte = Serial.read();
+    if (newByte == '\r' || newByte == '\n')  // Only considered "\r\n" and "\n"
+    {
+      //Do nothing
+    }
+    else if (newByte == '[')
+    {
+      bTeensyRecving = true;
+    }
+    else if (newByte == ']')
+    {
+      if (bTeensyRecving)  // which should be the case
+      {
+#ifdef DEBUGGING
+        // Feedback
+        Serial.println("Received.");  // Received command
+#endif
+        // Parse trial information
+        pBuffer = 0;
+        if (ParseCmd(buffer))
+        {
+          Serial.flush();
+#ifdef DEBUGGING
+          Serial.println("Go.");
+#endif
+          memset(buffer, '\0', BUFSIZE);
+          bTeensyRecving = false;
+          bTeensyExecing = true;
+#ifdef DEBUGGING
+          // Feedback
+          Serial.println("Parsed.");  // Parsed command
+#endif
+          // Cmd loaded
+          return;
+        }
+        else
+        {
+#ifdef DEBUGGING
+          Serial.println("No go.");
+#endif
+        }
+      }
+    }
+    else
+    {
+      if (bTeensyRecving)  // which should be the case
+      {
+        buffer[pBuffer] = char(newByte);
+        pBuffer++;
+        if (pBuffer == BUFSIZE)
+        {
+#ifdef DEBUGGING
+          Serial.println("Error: Input parameter longer than buffer size.");
+#endif
+          //Serial.flush();
+          return;
+        }
+      }
+    }
+  }
+  return;
+}
+
+boolean ParseCmd(char* buffer)
+{
+  nPeriods = atoi(strtok(buffer, ","));
+  periodOn_ms = atoi(strtok(NULL, ","));
+  periodOff_ms = atoi(strtok(NULL, ","));
+  return true;
+}
+
+void ExecCmd()
+{
+  // Execute command
+  for (int i = 0; i < nPeriods; i++)
+  {
+    digitalWrite(pinLED, LED_ON);
+    delay(periodOn_ms);
+    digitalWrite(pinLED, LED_OFF);
+    delay(periodOff_ms);
+  }
+
+  // Command executed
+  bTeensyExecing = false;
+
+#ifdef DEBUGGING
+  //Feedback
+  Serial.println(".");
+#endif
+  return;
 }
 
